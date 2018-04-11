d437e0674a269c5a5160c326079f696e26dcac18
Victor de la Cruz <vcruz@amentum.net>
2018-04-11 18:04:56 -0500

    ** custom logging

--- src/main/scala/kie/BotResponseEngine.scala
+++ src/main/scala/kie/BotResponseEngine.scala
@@ -2,13 +2,18 @@ package kie
 
 import java.util
 
-object BotResponseEngine {
+import com.typesafe.scalalogging.Logger
+import config.logger.CustomAgendaEventListener
+import org.slf4j.LoggerFactory
 
+object BotResponseEngine {
+  val logger = Logger(LoggerFactory.getLogger(BotResponseEngine.getClass))
   def determineBotResponse(process:MessageResponse): List[AnyRef] = {
     val session = Kie.newSession
     val elegibleResponses = new util.ArrayList[String]()
     session.setGlobal("elegibleResponses",elegibleResponses)
     session.insert(process)
+    session.addEventListener(new CustomAgendaEventListener())
     session.fireAllRules()
     session.dispose()
     elegibleResponses.toArray.toList
